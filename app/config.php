dfdbb44ba6dc231a55f875ea4e3853eb71b2407c
James Duffy <james@cafeduff.com>
2015-01-22 22:44:29 -0800

    whoops. lets add the config file back

---
+++ app/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('CONSUMER_KEY', getenv('twitter_consumer_key'));
+define('CONSUMER_SECRET', getenv('twitter_consumer_secret'));
+define('OAUTH_CALLBACK', "http://markov.cafeduff.com/twittercallback");
